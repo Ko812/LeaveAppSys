2f0f214688fa7d154cca47f0a363d697ff070af2
Ko812 <appDeveloper@initkopico.com>
2024-06-10 21:22:51 +0800

    initial commit

---
+++ src/main/java/sg/nus/iss/javaspring/las/LeaveAppSystemApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.javaspring.las;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveAppSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveAppSystemApplication.class, args);
+	}
+
+}

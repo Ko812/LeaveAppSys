abe73f2af8f2dd4be4cb8c61a2d9b2ea2d4aded5
SunNy1one <e1335038@u.nus.edu>
2024-06-15 15:51:11 +0800

    Add ManagerController Model-LeaveStatus LeaveStatusRepository ManagerService and impl

--- src/main/java/sg/nus/iss/com/Leaveapp/model/LeaveStatus.java
+++ src/main/java/sg/nus/iss/com/Leaveapp/model/LeaveStatus.java
@@ -1,5 +1,34 @@
 package sg.nus.iss.com.Leaveapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+//import lombok.Getter;
+//import lombok.Setter;
+
+//@Entity
+//@Getter@Setter
+////@Table(name = "?")
+//public class LeaveStatus {
+//	//Applied, Cancelled, Approved, Rejected, Deleted
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Long id;
+//	
+//	@ManyToOne
+//	@JoinColumn(name = "LeaveApplication_id")
+//
+//	private Leave leaveApplication;
+//
+//	private String status;
+//
+//	private String comment;
+//
+//
+//}
 public enum LeaveStatus {
-	Applied, Cancelled, Approved, Rejected, Deleted 
+  Applied, Cancelled, Approved, Rejected, Deleted
 }
